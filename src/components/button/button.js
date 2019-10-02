14b0e7b00984c02a29857a92caf3e7f0901dae27
Dmitry <dmzebrov@gmail.com>
2019-10-02 19:46:34 +0400

    Removed fragment declaring where it isn't necessary

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -6,25 +6,21 @@ class Button extends React.Component {
   render() {
     if (this.props.buttonType === "input") {
       return (
-        <>
-          <input
-            className={"button " + this.props.classNames}
-            type="submit"
-            value={this.props.value}
-          />
-        </>
+        <input
+          className={"button " + this.props.classNames}
+          type="submit"
+          value={this.props.value}
+        />
       );
     }
     if (!this.props.buttonType || this.props.buttonType === "button") {
       return (
-        <>
-          <button
-            className={"button " + this.props.classNames}
-            onClick={this.props.handleClick}
-          >
-            {this.props.children}
-          </button>
-        </>
+        <button
+          className={"button " + this.props.classNames}
+          onClick={this.props.handleClick}
+        >
+          {this.props.children}
+        </button>
       );
     }
   }
